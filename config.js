71212f9fec4d45415597ed69d78af9c48687101e
Priyansh Bhardwaj <priyanshbhj@gmail.com>
2018-08-01 00:30:56 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '229447814379191',
+        clientSecret: '7cd308277602289306d3e37861f3db67'
+    }
 }
